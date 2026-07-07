68b0e8225160e8c1323946f9a18a8089bb84b42d
Luis Salvador <saccec@gmail.com>
2026-07-06 19:33:44 -0500

    Fix: cambio al 85%

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
 function calcularDescuento(precio) {
-<<<<<<< HEAD
-	    return precio * 0.95;
-=======
-    return precio * 0.80;
->>>>>>> daac08b499eab356ecb6e89574661f8ae74b62c2
+    return precio * 0.85;
 }
